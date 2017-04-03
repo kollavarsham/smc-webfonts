cc2f130b5b85157df447adc301ead4646f93016a
Floyd Pink <imbleedingme@googlemail.com>
2017-04-03 00:25:19 -0400

    makes some style changes and adds jsfiddle with live, editable demos

--- scripts/script.js
+++ scripts/script.js
@@ -1,61 +1,89 @@
 (function () {
   var fontCollection = [
     {
-      fontFamily : 'Meera',
-      fontWeight : 'normal',
-      fontStyle  : 'normal'
+      fontFamily  : 'Meera',
+      fontWeight  : 'normal',
+      fontStyle   : 'normal',
+      cssFileName : 'meera',
+      jsFiddleId  : 'v4vyxLju'
     }, {
-      fontFamily : 'Rachana',
-      fontWeight : 'normal',
-      fontStyle  : 'normal'
+      fontFamily  : 'Rachana',
+      fontWeight  : 'normal',
+      fontStyle   : 'normal',
+      cssFileName : 'rachana',
+      jsFiddleId  : 'mapzaymx'
     }, {
-      fontFamily : 'Dyuthi',
-      fontWeight : 'normal',
-      fontStyle  : 'normal'
+      fontFamily  : 'Dyuthi',
+      fontWeight  : 'normal',
+      fontStyle   : 'normal',
+      cssFileName : 'dyuthi',
+      jsFiddleId  : '1c791dfe'
     }, {
-      fontFamily : 'RaghuMalayalam',
-      fontWeight : 'normal',
-      fontStyle  : 'normal'
+      fontFamily  : 'RaghuMalayalam',
+      fontWeight  : 'normal',
+      fontStyle   : 'normal',
+      cssFileName : 'raghumalayalam',
+      jsFiddleId  : 'f1L1jy2z'
     }, {
-      fontFamily : 'Chilanka',
-      fontWeight : 'normal',
-      fontStyle  : 'normal'
+      fontFamily  : 'Chilanka',
+      fontWeight  : 'normal',
+      fontStyle   : 'normal',
+      cssFileName : 'chilanka',
+      jsFiddleId  : 'tp2m8L9p'
     }, {
-      fontFamily : 'Keraleeyam',
-      fontWeight : 'bold',
-      fontStyle  : 'normal'
+      fontFamily  : 'Keraleeyam',
+      fontWeight  : 'bold',
+      fontStyle   : 'normal',
+      cssFileName : 'keraleeyam',
+      jsFiddleId  : 'a6uqj4eL'
     }, {
-      fontFamily : 'Manjari',
-      fontWeight : 'normal',
-      fontStyle  : 'normal'
+      fontFamily  : 'Manjari',
+      fontWeight  : 'normal',
+      fontStyle   : 'normal',
+      cssFileName : 'manjari',
+      jsFiddleId  : '133d3ajo'
     }, {
-      fontFamily : 'Manjari',
-      fontWeight : 100,
-      fontStyle  : 'normal'
+      fontFamily  : 'Manjari',
+      fontWeight  : 100,
+      fontStyle   : 'normal',
+      cssFileName : 'manjari',
+      jsFiddleId  : 'h4mmru5e'
     }, {
-      fontFamily : 'Suruma',
-      fontWeight : 500,
-      fontStyle  : 'normal'
+      fontFamily  : 'Suruma',
+      fontWeight  : 500,
+      fontStyle   : 'normal',
+      cssFileName : 'suruma',
+      jsFiddleId  : 'Lv8dchff'
     }, {
-      fontFamily : 'Rachana',
-      fontWeight : 'bold',
-      fontStyle  : 'normal'
+      fontFamily  : 'Rachana',
+      fontWeight  : 'bold',
+      fontStyle   : 'normal',
+      cssFileName : 'rachana',
+      jsFiddleId  : '1f7nxt16'
     }, {
-      fontFamily : 'Uroob',
-      fontWeight : 'bold',
-      fontStyle  : 'normal'
+      fontFamily  : 'Uroob',
+      fontWeight  : 'bold',
+      fontStyle   : 'normal',
+      cssFileName : 'uroob',
+      jsFiddleId  : 'pLq5qz9j'
     }, {
-      fontFamily : 'Karumbi',
-      fontWeight : 'normal',
-      fontStyle  : 'normal'
+      fontFamily  : 'Karumbi',
+      fontWeight  : 'normal',
+      fontStyle   : 'normal',
+      cssFileName : 'karumbi',
+      jsFiddleId  : '7d3okgq9'
     }, {
-      fontFamily : 'AnjaliOldLipi',
-      fontWeight : 'normal',
-      fontStyle  : 'normal'
+      fontFamily  : 'AnjaliOldLipi',
+      fontWeight  : 'normal',
+      fontStyle   : 'normal',
+      cssFileName : 'anjali',
+      jsFiddleId  : '9ajq9cun'
     }, {
-      fontFamily : 'Manjari',
-      fontWeight : 'bold',
-      fontStyle  : 'normal'
+      fontFamily  : 'Manjari',
+      fontWeight  : 'bold',
+      fontStyle   : 'normal',
+      cssFileName : 'manjari',
+      jsFiddleId  : '3c3vuuz6'
     }
   ];
 
@@ -82,28 +110,6 @@
   };
 
   var switcher = document.querySelector('#switcher');
-  fonts.forEach(function (f) {
-    var fontElement = document.createElement('div');
-    fontElement.id = f.properName + '-button';
-    fontElement.className = 'switcher-font';
-    var normalizedName = f.name.replace(/\s\(normal\)/, '');
-    fontElement.innerText = normalizedName;
-    fontElement.onclick = function () {
-      var nameElement = document.querySelector('#font-name');
-      nameElement.innerText = normalizedName;
-
-      var fontContainerElement = document.querySelector('.font-container');
-      fontContainerElement.style.cssText = 'font-family: \'' + f.font.fontFamily + '\'; font-weight: ' + f.font.fontWeight + '; font-style: normal;';
-
-      var usageElement = document.querySelector('#usage');
-      usageElement.innerText = '.your-style {\n  font-family: \'' + f.font.fontFamily + '\';\n  font-weight: ' + f.font.fontWeight + ';\n  font-style: normal;\n}';
-
-      window.location.hash = '#' + f.properName;
-
-      makeActive('.switcher-font', this);
-    };
-    switcher.appendChild(fontElement);
-  });
 
   var openSwitcher = document.querySelector('#open-switcher');
   openSwitcher.onclick = function () {
@@ -114,6 +120,46 @@
   closeSwitcher.onclick = function () {
     switcher.style.display = 'none';
   };
+
+  fonts.forEach(function (f) {
+    var fontElement = document.createElement('div');
+    fontElement.id = f.properName + '-button';
+    fontElement.className = 'switcher-font';
+    var normalizedName = f.name.replace(/\s\(normal\)/, '');
+    fontElement.innerText = normalizedName;
+    fontElement.onclick = function () {
+      var nameElement = document.querySelector('#font-name');
+      nameElement.innerText = normalizedName;
+
+      var fontCssText = 'font-family: \'' + f.font.fontFamily + '\'; font-weight: ' + f.font.fontWeight + '; font-style: normal;';
+      var fontContainerElement = document.querySelector('.font-container');
+      fontContainerElement.style.cssText = fontCssText;
+
+      var tabHeadContainerElement = document.querySelector('.tab-head-container');
+      tabHeadContainerElement.style.cssText = fontCssText;
+
+      var usageElement = document.querySelector('#usage');
+      usageElement.innerText = '<!-- HTML (within the HEAD element) -->\n<link rel="stylesheet" href="https://cdn.jsdelivr.net/smc-webfonts/0.0.1/fonts/' + f.font.cssFileName + '.min.css">\n\n/* CSS */\n.your-style {\n  font-family : \'' + f.font.fontFamily + '\';\n  font-weight : ' + f.font.fontWeight + ';\n  font-style  : normal;\n}';
+
+      window.location.hash = '#' + f.properName;
+
+      makeActive('.switcher-font', this);
+
+      var jsFiddleContainer = document.querySelector('#jsFiddle-container');
+      while (jsFiddleContainer.firstChild) {
+        jsFiddleContainer.removeChild(jsFiddleContainer.firstChild);
+      }
+      var jsFiddle = document.createElement('script');
+      jsFiddle.setAttribute('async', '');
+      jsFiddle.setAttribute('src', '//jsfiddle.net/FloydPink/' + f.font.jsFiddleId + '/embed/result,css,html/');
+
+      jsFiddleContainer.appendChild(jsFiddle);
+
+      closeSwitcher.onclick.call(closeSwitcher);
+    };
+    switcher.appendChild(fontElement);
+
+  });
 
   document.querySelectorAll('.tab-head').forEach(function (h) {
     h.onclick = function () {
